10d02a00492e3ea871f6be3630db254ac3bd2d5d
y924636 <162668096+y924636@users.noreply.github.com>
2024-08-18 21:55:12 -0700

    json

--- myApp.js
+++ myApp.js
@@ -3,12 +3,22 @@ let app = express();
 
 console.log('hello world!')
 
-app.get('/',function(req,res){
-    res.send('hello express')
+// app.get('/',function(req,res){
+//     res.send('hello express')
+// })
+app.use(express.static(__dirname+'/public/'))
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 
+app.get('/json',(req,res)=>{
+//     res.send( JSON.stringify({'message':'hello json'}).substr() )
+ let obj = {'message':'hi json'}
+res.send(obj.message)
 
+
+})
 
 
 
